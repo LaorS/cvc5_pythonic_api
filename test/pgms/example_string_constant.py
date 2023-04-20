3278d1807519fb8bc89f4031bd88538d3155b82b
LaorS <laorsp7@gmail.com>
2023-04-20 14:45:28 +0300

    need ti remove concat

--- test/pgms/example_string_constant.py
+++ test/pgms/example_string_constant.py
@@ -1,9 +1,10 @@
 from cvc5_pythonic_api import *
+#from z3 import *
 a = String('A')
 const = StringVal('hello')
 solve(a == const)
 b = String('B')
-solve(a == a.concat(b), a == const)
-solve(a == concat(a,b),b == const)
-solve(a == a+const)
+#solve(a == a.concat(b), a == const)
+#solve(a == concat(a,b),b == const)
+solve(a == a+const +a)
 solve(a == const+a)
